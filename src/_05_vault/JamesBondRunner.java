8d610e13d787c69c5d3d0c572bc9002ba07a430a
League Student <league@iMac-24.attlocal.net>
2019-10-31 16:38:42 -0700

    Vault(IP)
    :smile:

---
+++ src/_05_vault/JamesBondRunner.java
@@ -0,0 +1,11 @@
+package _05_vault;
+
+public class JamesBondRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vault vault= new Vault();
+System.out.println(vault.tryCode(5));
+	}
+
+}
--- src/_05_vault/Vault.java
+++ src/_05_vault/Vault.java
@@ -1,5 +1,18 @@
 package _05_vault;
 
 public class Vault {
+int secretCode=1;
 
+public boolean tryCode(int code) {
+	if(code==secretCode) {
+		return true;
+		
+			
+		}
+	else {
+		return false;
+	}
+	}
 }
+
+
